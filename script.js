46f533a4b0c996d1955f78e45f1f9fb7be833e97
Niki Wacherhausen <nikiwacherhausen@laptop-dljbmfjb.kea.local>
2019-06-14 14:24:07 +0200

    about me er basically færdig. lavet fold-ud boks med JS. ændret fonts, så de passer med fold-ud boks. skrevet lidt CV. Gjort klar til at linke til masonry-gallery.

--- script.js
+++ script.js
@@ -2,5 +2,11 @@ window.addEventListener("load", sidenVises);
 
 function sidenVises() {
     console.log('sidenVises');
+    document.querySelector("#mere").addEventListener("click", readMore);
+}
 
+function readMore() {
+    console.log('readMore:');
+    document.querySelector("#box").classList.toggle("stor");
+    document.querySelector("#box").classList.toggle("lille");
 }
